ec0c1ef63b77d0354f2efd4954fd4d26886645cf
Andreas Schneider <mail@cynapses.org>
2009-03-08 14:53:57 +0000

    Add NSIS support.
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/libssh/trunk@251 7dcaeef0-15fb-0310-b436-a5af3365683c

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -7,6 +7,9 @@ include(InstallRequiredSystemLibraries)
 set(CPACK_PACKAGE_NAME ${APPLICATION_NAME})
 set(CPACK_PACKAGE_VENDOR "${APPLICATION_NAME} developers")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "The SSH library")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "libssh")
+set(CPACK_PACKAGING_INSTALL_PREFIX "libssh")
+set(CPACK_PACKAGE_VENDOR "The SSH Libaray Development Team")
 
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/COPYING")
 
@@ -16,12 +19,35 @@ set(CPACK_PACKAGE_VERSION_MINOR "2")
 set(CPACK_PACKAGE_VERSION_PATCH "90")
 set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
 
-set(CPACK_GENERATOR "TGZ")
+set(CPACK_GENERATOR "NSIS")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 
 ### source package settings
 set(CPACK_SOURCE_GENERATOR "TGZ")
 set(CPACK_SOURCE_IGNORE_FILES "~$;[.]swp$;/[.]svn/;/[.]git/;.gitignore;/build/;tags;cscope.*")
 set(CPACK_SOURCE_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
+
+### nsis specific
+set(CPACK_NSIS_DISPLAY_NAME "The SSH Library")
+#set(CPACK_NSIS_COMPRESSOR "/SOLID lzma")
+
+# see http://www.cmake.org/Wiki/CMake:Component_Install_With_CPack
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "libssh")
+
+set(CPACK_COMPONENT_LIBRARIES_DISPLAY_NAME "Libraries")
+set(CPACK_COMPONENT_HEADERS_DISPLAY_NAME "C/C++ Headers")
+
+set(CPACK_COMPONENT_LIBRARIES_DESCRIPTION
+  "Libraries used to build programs which use libssh")
+set(CPACK_COMPONENT_HEADERS_DESCRIPTION
+  "C/C++ header files for use with libssh")
+
+set(CPACK_COMPONENT_HEADERS_DEPENDS libraries)
+
+#set(CPACK_COMPONENT_APPLICATIONS_GROUP "Runtime")
+#set(CPACK_COMPONENT_LIBRARIES_GROUP "Development")
+#set(CPACK_COMPONENT_HEADERS_GROUP "Development")
+
+set(CPACK_NSIS_MENU_LINKS "http://0xbadc0de.be/wiki/libssh:libssh" "libssh homepage")
 
 include(CPack)
