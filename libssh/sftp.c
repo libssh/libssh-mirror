7592d9f42cefdb666a1af2a011a8a00f9731ce2c
Andreas Schneider <mail@cynapses.org>
2009-04-23 11:38:55 +0000

    Reformat sftp_parse_attr().
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/libssh/trunk@594 7dcaeef0-15fb-0310-b436-a5af3365683c

--- libssh/sftp.c
+++ libssh/sftp.c
@@ -1108,17 +1108,20 @@ int buffer_add_attributes(BUFFER *buffer, SFTP_ATTRIBUTES *attr) {
 }
 
 
-SFTP_ATTRIBUTES *sftp_parse_attr(SFTP_SESSION *session, BUFFER *buf,int expectname){
-    switch(session->version){
-        case 4:
-            return sftp_parse_attr_4(session,buf,expectname);
-        case 3:
-            return sftp_parse_attr_3(session,buf,expectname);
-        default:
-            ssh_set_error(session->session,SSH_FATAL,"Version %d unsupported by client",session->server_version);
-            return NULL;
-    }
-    return NULL;
+SFTP_ATTRIBUTES *sftp_parse_attr(SFTP_SESSION *session, BUFFER *buf,
+    int expectname) {
+  switch(session->version) {
+    case 4:
+      return sftp_parse_attr_4(session, buf, expectname);
+    case 3:
+      return sftp_parse_attr_3(session, buf, expectname);
+    default:
+      ssh_set_error(session->session, SSH_FATAL,
+          "Version %d unsupported by client", session->server_version);
+      return NULL;
+  }
+
+  return NULL;
 }
 
 /* Get the version of the SFTP protocol supported by the server */
