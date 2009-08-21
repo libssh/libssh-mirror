330c2004a181f7ad3566aa980a8a501348358439
Andreas Schneider <mail@cynapses.org>
2009-08-21 10:17:58 +0200

    Make the functions public.

--- include/libssh/libssh.h
+++ include/libssh/libssh.h
@@ -444,8 +444,8 @@ LIBSSH_API int ssh_init(void);
 LIBSSH_API int ssh_finalize(void);
 
 /* misc.c */
-char *ssh_dirname (const char *path);
-char *ssh_basename (const char *path);
+LIBSSH_API char *ssh_dirname (const char *path);
+LIBSSH_API char *ssh_basename (const char *path);
 
 /* messages.c */
 typedef struct ssh_message SSH_MESSAGE;
