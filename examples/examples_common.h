f90ae73b6d0d8c6993a200045770344ce5252a92
Aris Adamantiadis <aris@0xbadc0de.be>
2009-09-06 12:54:57 +0300

    Refactored the connect_ssh example function
    into a new file

--- examples/examples_common.h
+++ examples/examples_common.h
@@ -13,9 +13,10 @@ clients must be made or how a client should react.
 #ifndef EXAMPLES_COMMON_H_
 #define EXAMPLES_COMMON_H_
 
+#include <libssh/libssh.h>
 int authenticate_console(ssh_session session);
 int authenticate_kbdint(ssh_session session);
 int verify_knownhost(ssh_session session);
-
+ssh_session connect_ssh(const char *hostname, const char *user, int verbosity);
 
 #endif /* EXAMPLES_COMMON_H_ */
