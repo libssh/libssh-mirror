a479b302983aae2878af0cb50eac141ede6cd942
Aris Adamantiadis <aris@0xbadc0de.be>
2009-10-17 17:54:39 +0200

    Verify existence of callback before using them

--- libssh/keyfiles.c
+++ libssh/keyfiles.c
@@ -692,7 +692,7 @@ ssh_private_key privatekey_from_file(ssh_session session, const char *filename,
       break;
     case TYPE_RSA:
       if (passphrase == NULL) {
-	if (session->callbacks->auth_function) {
+	if (session->callbacks && session->callbacks->auth_function) {
 		auth_cb = session->callbacks->auth_function;
 		auth_ud = session->callbacks->userdata;
 #ifdef HAVE_LIBGCRYPT
