492d1f24c834a5d1320dfe9699c6fda65ab3e1ba
Andreas Schneider <mail@cynapses.org>
2010-02-13 12:47:28 +0100

    Set CFLAGS correctly.

--- cmake/Modules/DefineCompilerFlags.cmake
+++ cmake/Modules/DefineCompilerFlags.cmake
@@ -3,27 +3,27 @@
 include(CheckCCompilerFlag)
 
 if (UNIX AND NOT WIN32)
-  if (CMAKE_COMPILER_IS_GNUCC)
+  if (${CMAKE_C_COMPILER_ID} MATCHES GNU)
     # add -Wconversion ?
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99 -pedantic -Wall -Wextra -Wshadow -Wmissing-prototypes -Wdeclaration-after-statement -Wunused -Wfloat-equal -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute")
 
     # with -fPIC
     check_c_compiler_flag("-fPIC" WITH_FPIC)
     if (WITH_FPIC)
-      add_definitions(-fPIC)
+      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
     endif (WITH_FPIC)
 
     check_c_compiler_flag("-fstack-protector" WITH_STACK_PROTECTOR)
     if (WITH_STACK_PROTECTOR)
-      add_definitions(-fstack-protector)
+      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fstack-protector")
     endif (WITH_STACK_PROTECTOR)
 
     check_c_compiler_flag("-D_FORTIFY_SOURCE=2" WITH_FORTIFY_SOURCE)
     if (WITH_FORTIFY_SOURCE)
-      add_definitions(-D_FORTIFY_SOURCE=2)
+      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_FORTIFY_SOURCE=2")
     endif (WITH_FORTIFY_SOURCE)
 
-  endif (CMAKE_COMPILER_IS_GNUCC)
+  endif (${CMAKE_C_COMPILER_ID} MATCHES GNU)
 
   if (CMAKE_SIZEOF_VOID_P MATCHES "8")
    # with large file support
@@ -48,12 +48,12 @@ if (UNIX AND NOT WIN32)
  endif (CMAKE_SIZEOF_VOID_P MATCHES "8")
  if (_lfs_CFLAGS)
    string(REGEX REPLACE "[\r\n]" " " "${_lfs_CFLAGS}" "${${_lfs_CFLAGS}}")
-   add_definitions(${_lfs_CFLAGS})
+   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_lfs_CFLAGS}")
  endif (_lfs_CFLAGS)
 
 endif (UNIX AND NOT WIN32)
 
 # suppress warning about "deprecated" functions
 if (MSVC)
-  add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_CRT_SECURE_NO_WARNINGS")
 endif (MSVC)
