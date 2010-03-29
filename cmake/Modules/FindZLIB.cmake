2c16a3c0e88ae63491995759e153a7f033de1fda
Andreas Schneider <mail@cynapses.org>
2010-03-29 23:20:43 +0200

    Fixed ZLIB cmake module.

--- cmake/Modules/FindZLIB.cmake
+++ cmake/Modules/FindZLIB.cmake
@@ -6,7 +6,7 @@
 #  ZLIB_LIBRARIES - Link these to use ZLIB
 #  ZLIB_DEFINITIONS - Compiler switches required for using ZLIB
 #
-#  Copyright (c) 2009 Andreas Schneider <mail@cynapses.org>
+#  Copyright (c) 2009-2010 Andreas Schneider <mail@cynapses.org>
 #
 #  Redistribution and use is allowed according to the terms of the New
 #  BSD license.
@@ -27,8 +27,8 @@ else (ZLIB_LIBRARIES AND ZLIB_INCLUDE_DIRS)
       /usr/local/include
       /opt/local/include
       /sw/include
+      /usr/lib/sfw/include
   )
-  mark_as_advanced(ZLIB_INCLUDE_DIR)
 
   find_library(Z_LIBRARY
     NAMES
@@ -40,37 +40,23 @@ else (ZLIB_LIBRARIES AND ZLIB_INCLUDE_DIRS)
       /usr/local/lib
       /opt/local/lib
       /sw/lib
+      /usr/sfw/lib/64
+      /usr/sfw/lib
   )
-  mark_as_advanced(Z_LIBRARY)
-
-  if (Z_LIBRARY)
-    set(Z_FOUND TRUE)
-  endif (Z_LIBRARY)
 
   set(ZLIB_INCLUDE_DIRS
     ${ZLIB_INCLUDE_DIR}
   )
 
-  if (Z_FOUND)
+  if (Z_LIBRARY)
     set(ZLIB_LIBRARIES
-      ${ZLIB_LIBRARIES}
-      ${Z_LIBRARY}
+        ${ZLIB_LIBRARIES}
+        ${Z_LIBRARY}
     )
-  endif (Z_FOUND)
+  endif (Z_LIBRARY)
 
-  if (ZLIB_INCLUDE_DIRS AND ZLIB_LIBRARIES)
-     set(ZLIB_FOUND TRUE)
-  endif (ZLIB_INCLUDE_DIRS AND ZLIB_LIBRARIES)
-
-  if (ZLIB_FOUND)
-    if (NOT ZLIB_FIND_QUIETLY)
-      message(STATUS "Found ZLIB: ${ZLIB_LIBRARIES}")
-    endif (NOT ZLIB_FIND_QUIETLY)
-  else (ZLIB_FOUND)
-    if (ZLIB_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find ZLIB")
-    endif (ZLIB_FIND_REQUIRED)
-  endif (ZLIB_FOUND)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(ZLIB DEFAULT_MSG ZLIB_LIBRARIES ZLIB_INCLUDE_DIRS)
 
   # show the ZLIB_INCLUDE_DIRS and ZLIB_LIBRARIES variables only in the advanced view
   mark_as_advanced(ZLIB_INCLUDE_DIRS ZLIB_LIBRARIES)
