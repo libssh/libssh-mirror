c8d978f4e9f28f6bb0a682ab423fd4820299d2fd
Andreas Schneider <mail@cynapses.org>
2010-06-20 10:29:25 +0200

    ctest: Update config file for new cdash board.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,10 +3,9 @@ set(UPDATE_TYPE "true")
 set(MEMORYCHECK_SUPPRESSIONS_FILE ${CMAKE_SOURCE_DIR}/tests/valgrind.supp)
 
 set(CTEST_PROJECT_NAME "libssh")
-set(CTEST_NIGHTLY_START_TIME "04:00:00 CET")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=${CTEST_PROJECT_NAME}")
+set(CTEST_DROP_SITE "test.libssh.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=libssh")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
