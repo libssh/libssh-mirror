fd53ebc7b62415d815edf7ad256fe8a17f6f813f
Andreas Schneider <asn@cynapses.org>
2010-07-13 17:25:48 +0200

    build: Define values for printf functions.

--- config.h.cmake
+++ config.h.cmake
@@ -38,26 +38,26 @@
 /*************************** FUNCTIONS ***************************/
 
 /* Define to 1 if you have the `vsnprintf' function. */
-#cmakedefine HAVE_VSNPRINTF
+#cmakedefine HAVE_VSNPRINTF 1
 
 /* Define to 1 if you have the `_vsnprintf' function. */
-#cmakedefine HAVE__VSNPRINTF
+#cmakedefine HAVE__VSNPRINTF 1
 
 /* Define to 1 if you have the `_vsnprintf_s' function. */
-#cmakedefine HAVE__VSNPRINTF_S
+#cmakedefine HAVE__VSNPRINTF_S 1
 
 /* Define to 1 if you have the `snprintf' function. */
-#cmakedefine HAVE_SNPRINTF
+#cmakedefine HAVE_SNPRINTF 1
 
 /* Define to 1 if you have the `_snprintf' function. */
-#cmakedefine HAVE__SNPRINTF
+#cmakedefine HAVE__SNPRINTF 1
 
 /* Define to 1 if you have the `_snprintf_s' function. */
-#cmakedefine HAVE__SNPRINTF_S
+#cmakedefine HAVE__SNPRINTF_S 1
 
 
 /* Define to 1 if you have the `strncpy' function. */
-#cmakedefine HAVE_STRNCPY
+#cmakedefine HAVE_STRNCPY 1
 
 /* Define to 1 if you have the `cfmakeraw' function. */
 #cmakedefine HAVE_CFMAKERAW 1
