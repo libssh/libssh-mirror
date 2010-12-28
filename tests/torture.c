635a263f64ea39e2fed9e95be4c3cc425aee9302
Andreas Schneider <asn@cynapses.org>
2010-12-28 13:43:32 +0100

    tests: Migrate the torture library to cmockery.

--- tests/torture.c
+++ tests/torture.c
@@ -2,52 +2,12 @@
 
 #include <stdio.h>
 
-void torture_create_case(Suite *s, const char *name, TFun function) {
-  TCase *tc_new = tcase_create(name);
-  tcase_set_timeout(tc_new, 30);
-  suite_add_tcase (s, tc_new);
-  tcase_add_test(tc_new, function);
-}
+static int verbosity = 0;
 
-void torture_create_case_fixture(Suite *s, const char *name, TFun function, void (*setup)(void), void (*teardown)(void)) {
-  TCase *tc_new = tcase_create(name);
-  tcase_add_checked_fixture(tc_new, setup, teardown);
-  tcase_set_timeout(tc_new, 30);
-  suite_add_tcase (s, tc_new);
-  tcase_add_test(tc_new, function);
-}
-
-void torture_create_case_timeout(Suite *s, const char *name, TFun function, int timeout) {
-  TCase *tc_new = tcase_create(name);
-  tcase_set_timeout(tc_new, timeout);
-  suite_add_tcase (s, tc_new);
-  tcase_add_test(tc_new, function);
-}
-
-static int verbosity=0;
 int torture_libssh_verbosity(void){
   return verbosity;
 }
 
 int main(int argc, char **argv) {
-  Suite *s = NULL;
-  SRunner *sr = NULL;
-  struct argument_s arguments;
-  int nf;
-
-  memset(&arguments,0,sizeof(struct argument_s));
-
-  torture_cmdline_parse(argc, argv, &arguments);
-  verbosity=arguments.verbose;
-  s = torture_make_suite();
-
-  sr = srunner_create(s);
-  if (arguments.nofork) {
-    srunner_set_fork_status(sr, CK_NOFORK);
-  }
-  srunner_run_all(sr, CK_VERBOSE);
-  nf = srunner_ntests_failed(sr);
-  srunner_free(sr);
-
-  return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    return torture_run_tests();
 }
--- tests/torture.h
+++ tests/torture.h
@@ -7,33 +7,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <check.h>
-
-/* Used by main to communicate with parse_opt. */
-struct argument_s {
-  char *args[2];
-  int nofork;
-  int verbose;
-};
-
-void torture_cmdline_parse(int argc, char **argv, struct argument_s *arguments);
-
-/* create_case() with timeout of 30seconds (default) */
-void torture_create_case(Suite *s, const char *name, TFun function);
-
-/* create_case() with timeout of 30seconds (default) and fixture */
-void torture_create_case_fixture(Suite *s, const char *name, TFun function,
-    void (*setup)(void), void (*teardown)(void));
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <google/cmockery.h>
 
 /*
- * create_case_timeout() allow to specific a specific timeout - intended for
- * breaking testcases which needs longer then 30seconds (default)
- */
-void torture_create_case_timeout(Suite *s, const char *name, TFun function,
-    int timeout);
-
-/*
- * returns the verbosity level asked by user
+ * Returns the verbosity level asked by user
  */
 int torture_libssh_verbosity(void);
 
@@ -41,7 +21,6 @@ int torture_libssh_verbosity(void);
 /*
  * This function must be defined in every unit test file.
  */
-Suite *torture_make_suite(void);
-
+int torture_run_tests(void);
 
 #endif /* _TORTURE_H */
