17f592d4dd25eae98b1a8378625adc454976fcb6
Andreas Schneider <asn@cynapses.org>
2010-12-28 13:41:33 +0100

    tests: Start to migrate tests to cmockery.
    https://code.google.com/p/cmockery/wiki/Cmockery_Unit_Testing_Framework

--- cmake/Modules/AddCMockeryTest.cmake
+++ cmake/Modules/AddCMockeryTest.cmake
@@ -1,7 +1,7 @@
 # - ADD_CHECK_TEST(test_name test_source linklib1 ... linklibN)
 
-# Copyright (c) 2007, Daniel Gollub, <dgollub@suse.de>
-# Copyright (c) 2007, Andreas Schneider, <mail@cynapses.org>
+# Copyright (c) 2007      Daniel Gollub <dgollub@suse.de>
+# Copyright (c) 2007-2010 Andreas Schneider <asn@cynapses.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
@@ -14,9 +14,8 @@ set(CMAKE_SHARED_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE 
 set(CMAKE_MODULE_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Linker Flags")
 set(CMAKE_EXEC_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Linker Flags")
 
-function (ADD_CHECK_TEST _testName _testSource)
-  add_executable(${_testName} ${_testSource})
-  target_link_libraries(${_testName} ${ARGN})
-  add_test(${_testName} ${CMAKE_CURRENT_BINARY_DIR}/${_testName})
-endfunction (ADD_CHECK_TEST)
-
+function (ADD_CMOCKERY_TEST _testName _testSource)
+    add_executable(${_testName} ${_testSource})
+    target_link_libraries(${_testName} ${ARGN})
+    add_test(${_testName} ${CMAKE_CURRENT_BINARY_DIR}/${_testName})
+endfunction (ADD_CMOCKERY_TEST)
---
+++ cmake/Modules/FindCMockery.cmake
@@ -0,0 +1,59 @@
+# - Try to find CMockery
+# Once done this will define
+#
+#  CMOCKERY_FOUND - system has CMockery
+#  CMOCKERY_INCLUDE_DIRS - the CMockery include directory
+#  CMOCKERY_LIBRARIES - Link these to use CMockery
+#  CMOCKERY_DEFINITIONS - Compiler switches required for using CMockery
+#
+#  Copyright (c) 2010 Andreas Schneider <asn@cryptomilk.org>
+#
+#  Redistribution and use is allowed according to the terms of the New
+#  BSD license.
+#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+
+if (CMOCKERY_LIBRARIES AND CMOCKERY_INCLUDE_DIRS)
+  # in cache already
+  set(CMOCKERY_FOUND TRUE)
+else (CMOCKERY_LIBRARIES AND CMOCKERY_INCLUDE_DIRS)
+
+  find_path(CMOCKERY_INCLUDE_DIR
+    NAMES
+      google/cmockery.h
+    PATHS
+      /usr/include
+      /usr/local/include
+      /opt/local/include
+      /sw/include
+  )
+
+  find_library(CMOCKERY_LIBRARY
+    NAMES
+      cmockery
+    PATHS
+      /usr/lib
+      /usr/local/lib
+      /opt/local/lib
+      /sw/lib
+  )
+
+  set(CMOCKERY_INCLUDE_DIRS
+    ${CMOCKERY_INCLUDE_DIR}
+  )
+
+  if (CMOCKERY_LIBRARY)
+    set(CMOCKERY_LIBRARIES
+        ${CMOCKERY_LIBRARIES}
+        ${CMOCKERY_LIBRARY}
+    )
+  endif (CMOCKERY_LIBRARY)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(CMockery DEFAULT_MSG CMOCKERY_LIBRARIES CMOCKERY_INCLUDE_DIRS)
+
+  # show the CMOCKERY_INCLUDE_DIRS and CMOCKERY_LIBRARIES variables only in the advanced view
+  mark_as_advanced(CMOCKERY_INCLUDE_DIRS CMOCKERY_LIBRARIES)
+
+endif (CMOCKERY_LIBRARIES AND CMOCKERY_INCLUDE_DIRS)
