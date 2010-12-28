df55918a41cc8a994bfcb955bd6d8ca068f5f62f
Andreas Schneider <asn@cynapses.org>
2010-12-28 16:59:07 +0100

    tests: Migrated torture_proxycommand to cmockery.

--- tests/client/torture_proxycommand.c
+++ tests/client/torture_proxycommand.c
@@ -3,52 +3,48 @@
 #include "torture.h"
 #include <libssh/libssh.h>
 #include "libssh/priv.h"
-ssh_session session;
 
-static void setup(void) {
-    session = ssh_new();
+static void setup(void **state) {
+    ssh_session session = ssh_new();
+
+    *state = session;
 }
 
-static void teardown(void) {
-    ssh_free(session);
+static void teardown(void **state) {
+    ssh_free(*state);
 }
 
-START_TEST (torture_options_set_proxycommand)
-{
+static void torture_options_set_proxycommand(void **state) {
+    ssh_session session = *state;
     int rc;
 
     rc = ssh_options_set(session, SSH_OPTIONS_HOST, "localhost");
-    ck_assert(rc == 0);
+    assert_true(rc == 0);
 
     rc = ssh_options_set(session, SSH_OPTIONS_PROXYCOMMAND, "nc localhost 22");
-    ck_assert(rc == 0);
+    assert_true(rc == 0);
     rc = ssh_connect(session);
-    ck_assert_msg(rc== SSH_OK,ssh_get_error(session));
+    assert_true(rc == SSH_OK);
 }
-END_TEST
 
-START_TEST (torture_options_set_proxycommand_notexist)
-{
+static void torture_options_set_proxycommand_notexist(void **state) {
+    ssh_session session = *state;
     int rc;
 
     rc = ssh_options_set(session, SSH_OPTIONS_HOST, "localhost");
-    ck_assert(rc == 0);
+    assert_true(rc == 0);
 
     rc = ssh_options_set(session, SSH_OPTIONS_PROXYCOMMAND, "this_command_does_not_exist");
-    ck_assert(rc == SSH_OK);
+    assert_true(rc == SSH_OK);
     rc = ssh_connect(session);
-    ck_assert_msg(rc== SSH_ERROR);
+    assert_true(rc == SSH_ERROR);
 }
-END_TEST
 
-Suite *torture_make_suite(void) {
-  Suite *s = suite_create("libssh_proxycommand");
+int torture_run_tests(void) {
+    const UnitTest tests[] = {
+        unit_test_setup_teardown(torture_options_set_proxycommand, setup, teardown),
+        unit_test_setup_teardown(torture_options_set_proxycommand_notexist, setup, teardown),
+    };
 
-  torture_create_case_fixture(s, "torture_options_set_proxycommand",
-          torture_options_set_proxycommand, setup, teardown);
-  torture_create_case_fixture(s, "torture_options_set_proxycommand_notexist",
-          torture_options_set_proxycommand_notexist, setup, teardown);
-
-
-  return s;
+    return run_tests(tests);
 }
