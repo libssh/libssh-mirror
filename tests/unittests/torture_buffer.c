6f65e6ae67d3431b87876b2cd4cc40383d5721e6
Andreas Schneider <asn@cynapses.org>
2010-12-28 13:44:14 +0100

    tests: Migrate torture_buffer to cmockery.

--- tests/unittests/torture_buffer.c
+++ tests/unittests/torture_buffer.c
@@ -4,39 +4,42 @@
 #define DEBUG_BUFFER
 #include "buffer.c"
 
-ssh_buffer buffer;
 #define LIMIT (8*1024*1024)
 
-static void setup(void) {
-    buffer=ssh_buffer_new();
+static void setup(void **state) {
+    ssh_buffer buffer;
+    buffer = ssh_buffer_new();
+    *state = (void *) buffer;
 }
 
-static void teardown(void) {
-  ssh_buffer_free(buffer);
+static void teardown(void **state) {
+    ssh_buffer_free(*state);
 }
 
-/* test if the continuously growing buffer size never exceeds 2 time its
+/*
+ * Test if the continuously growing buffer size never exceeds 2 time its
  * real capacity
  */
-START_TEST (torture_growing_buffer)
-{
+static void torture_growing_buffer(void **state) {
+  ssh_buffer buffer = *state;
   int i;
+
   for(i=0;i<LIMIT;++i){
     buffer_add_data(buffer,"A",1);
     if(buffer->used >= 128){
       if(buffer_get_rest_len(buffer) * 2 < buffer->allocated){
-        ck_assert(buffer_get_rest_len(buffer) * 2 >= buffer->allocated);
+        assert_true(buffer_get_rest_len(buffer) * 2 >= buffer->allocated);
       }
     }
   }
 }
-END_TEST
 
-/* test if the continuously growing buffer size never exceeds 2 time its
+/*
+ * Test if the continuously growing buffer size never exceeds 2 time its
  * real capacity, when we remove 1 byte after each call (sliding window)
  */
-START_TEST (torture_growing_buffer_shifting)
-{
+static void torture_growing_buffer_shifting(void **state) {
+  ssh_buffer buffer = *state;
   int i;
   unsigned char c;
   for(i=0; i<1024;++i){
@@ -47,51 +50,46 @@ START_TEST (torture_growing_buffer_shifting)
     buffer_add_data(buffer,"A",1);
     if(buffer->used >= 128){
       if(buffer_get_rest_len(buffer) * 4 < buffer->allocated){
-        ck_assert(buffer_get_rest_len(buffer) * 4 >= buffer->allocated);
+        assert_true(buffer_get_rest_len(buffer) * 4 >= buffer->allocated);
         return;
       }
     }
   }
 }
-END_TEST
 
-/* test the behavior of buffer_prepend_data
+/*
+ * Test the behavior of buffer_prepend_data
  */
-START_TEST (torture_buffer_prepend)
-{
+static void torture_buffer_prepend(void **state) {
+  ssh_buffer buffer = *state;
   uint32_t v;
   buffer_add_data(buffer,"abcdef",6);
   buffer_prepend_data(buffer,"xyz",3);
-  ck_assert_int_eq(buffer_get_rest_len(buffer),9);
-  ck_assert_int_eq(memcmp(buffer_get_rest(buffer), "xyzabcdef", 9), 0);
+  assert_int_equal(buffer_get_rest_len(buffer),9);
+  assert_int_equal(memcmp(buffer_get_rest(buffer), "xyzabcdef", 9), 0);
 // Now remove 4 bytes and see if we can replace them
   buffer_get_u32(buffer,&v);
-  ck_assert_int_eq(buffer_get_rest_len(buffer),5);
-  ck_assert_int_eq(memcmp(buffer_get_rest(buffer), "bcdef", 5), 0);
+  assert_int_equal(buffer_get_rest_len(buffer),5);
+  assert_int_equal(memcmp(buffer_get_rest(buffer), "bcdef", 5), 0);
   buffer_prepend_data(buffer,"aris",4);
-  ck_assert_int_eq(buffer_get_rest_len(buffer),9);
-  ck_assert_int_eq(memcmp(buffer_get_rest(buffer), "arisbcdef", 9), 0);
+  assert_int_equal(buffer_get_rest_len(buffer),9);
+  assert_int_equal(memcmp(buffer_get_rest(buffer), "arisbcdef", 9), 0);
   /* same thing but we add 5 bytes now */
   buffer_get_u32(buffer,&v);
-  ck_assert_int_eq(buffer_get_rest_len(buffer),5);
-  ck_assert_int_eq(memcmp(buffer_get_rest(buffer), "bcdef", 5), 0);
+  assert_int_equal(buffer_get_rest_len(buffer),5);
+  assert_int_equal(memcmp(buffer_get_rest(buffer), "bcdef", 5), 0);
   buffer_prepend_data(buffer,"12345",5);
-  ck_assert_int_eq(buffer_get_rest_len(buffer),10);
-  ck_assert_int_eq(memcmp(buffer_get_rest(buffer), "12345bcdef", 10), 0);
+  assert_int_equal(buffer_get_rest_len(buffer),10);
+  assert_int_equal(memcmp(buffer_get_rest(buffer), "12345bcdef", 10), 0);
 
 }
-END_TEST
 
+int torture_run_tests(void) {
+    const UnitTest tests[] = {
+        unit_test_setup_teardown(torture_growing_buffer, setup, teardown),
+        unit_test_setup_teardown(torture_growing_buffer_shifting, setup, teardown),
+        unit_test_setup_teardown(torture_buffer_prepend, setup, teardown),
+    };
 
-Suite *torture_make_suite(void) {
-  Suite *s = suite_create("libssh_buffer");
-
-  torture_create_case_fixture(s, "torture_growing_buffer",
-            torture_growing_buffer, setup, teardown);
-  torture_create_case_fixture(s, "torture_growing_buffer_shifting",
-            torture_growing_buffer_shifting, setup, teardown);
-  torture_create_case_fixture(s, "torture_buffer_prepend",
-            torture_buffer_prepend, setup, teardown);
-  return s;
+    return run_tests(tests);
 }
-
