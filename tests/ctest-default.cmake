da863405518962a5798f504acdb85257dfa8730d
Andreas Schneider <asn@cynapses.org>
2010-12-30 12:12:06 +0100

    cmake: Improved the ctest file.

--- tests/ctest-default.cmake
+++ tests/ctest-default.cmake
@@ -1,20 +1,29 @@
+## The directory to run ctest in.
 set(CTEST_DIRECTORY "$ENV{HOME}/workspace/tmp/dashboards/libssh")
 
+## The hostname of the machine
 set(CTEST_SITE "host.libssh.org")
-set(CTEST_BUILD_NAME "linux-gcc-default")
+## The buildname
+set(CTEST_BUILD_NAME "Linux_2.6-GCC_4.5-x86_64-default")
 
+## The Makefile generator to use
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+## The Build configuration to use.
 set(CTEST_BUILD_CONFIGURATION "Debug")
 
+## The build options for the project
 set(CTEST_BUILD_OPTIONS "-DWITH_TESTING=ON -DWITH_SSH1=ON -WITH_SFTP=ON -DWITH_SERVER=ON -DWITH_ZLIB=ON -DWITH_PCAP=ON -DWITH_GCRYPT=OFF")
 
-set(CTEST_SOURCE_DIRECTORY "${CTEST_DIRECTORY}/${CTEST_BUILD_NAME}/source")
-set(CTEST_BINARY_DIRECTORY "${CTEST_DIRECTORY}/${CTEST_BUILD_NAME}/build")
-
-set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE ${CMAKE_SOURCE_DIR}/tests/valgrind.supp)
 #set(CTEST_CUSTOM_MEMCHECK_IGNORE torture_rand)
 
+## The Model to set: Nightly, Continous, Experimental
+set(CTEST_MODEL "Experimental")
+
+## Wether to enable memory checking.
 set(WITH_MEMCHECK FALSE)
+
+## Wether to enable code coverage.
 set(WITH_COVERAGE FALSE)
 
 #######################################################################
@@ -23,7 +32,10 @@ if (WITH_COVERAGE AND NOT WIN32)
     set(CTEST_BUILD_CONFIGURATION "Profiling")
 endif (WITH_COVERAGE AND NOT WIN32)
 
-ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DIRECTORY}/${CTEST_BUILD_NAME}/source")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DIRECTORY}/${CTEST_BUILD_NAME}/build")
+
+set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE ${CMAKE_SOURCE_DIR}/tests/valgrind.supp)
 
 find_program(CTEST_GIT_COMMAND NAMES git)
 find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
@@ -40,7 +52,9 @@ set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} -DWITH_TESTING:BOOL=ON $
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTORY}\"")
 
-ctest_start("Nightly")
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+
+ctest_start(${CTEST_MODEL} TRACK ${CTEST_MODEL})
 ctest_update()
 ctest_configure()
 ctest_build()
