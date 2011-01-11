bb5f5281c007043231c4d95ce1309f1a6d999d33
Andreas Schneider <asn@cryptomilk.org>
2011-01-11 11:07:17 +0100

    build: Fixed build on Windows.

--- src/channels1.c
+++ src/channels1.c
@@ -24,11 +24,12 @@
 
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 #ifndef _WIN32
 #include <arpa/inet.h>
+#include <unistd.h>
 #endif
+
 #include "libssh/priv.h"
 #include "libssh/ssh1.h"
 #include "libssh/buffer.h"
