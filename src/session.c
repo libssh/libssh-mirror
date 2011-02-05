32e23a25da4e7d23bdede1cee4cadc6c80306c23
milo <milo@r0ot.me>
2011-02-05 12:34:35 +0100

    Reverse commit 076dfb82 for the server side

--- src/session.c
+++ src/session.c
@@ -395,6 +395,8 @@ int ssh_handle_packets(ssh_session session, int timeout) {
   enter_function();
   spoll_in=ssh_socket_get_poll_handle_in(session->socket);
   spoll_out=ssh_socket_get_poll_handle_out(session->socket);
+  if(session->server)
+    ssh_poll_add_events(spoll_in, POLLIN | POLLERR);
   ctx=ssh_poll_get_ctx(spoll_in);
   if(ctx==NULL){
   	ctx=ssh_poll_get_default_ctx(session);
