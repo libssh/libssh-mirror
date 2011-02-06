892ebf35f1d4600823e7db55fc6df67599b2f753
Andreas Schneider <asn@cryptomilk.org>
2011-02-06 14:50:33 +0100

    torture: Added GPL header.

--- tests/torture.h
+++ tests/torture.h
@@ -1,3 +1,26 @@
+/*
+ * torture.c - torture library for testing libssh
+ *
+ * This file is part of the SSH Library
+ *
+ * Copyright (c) 2008-2009 by Andreas Schneider <mail@cynapses.org>
+ *
+ * The SSH Library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * The SSH Library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the SSH Library; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
 #ifndef _TORTURE_H
 #define _TORTURE_H
 
@@ -13,6 +36,7 @@
 #include <google/cmockery.h>
 
 #include "libssh/priv.h"
+#include "libssh/sftp.h"
 
 /* Used by main to communicate with parse_opt. */
 struct argument_s {
