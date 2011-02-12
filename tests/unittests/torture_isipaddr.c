5d4bd5a21d9b903341c175696b0c747076672572
Andreas Schneider <asn@cryptomilk.org>
2011-02-12 19:14:30 +0100

    tests: Added ipv6 tests.

--- tests/unittests/torture_isipaddr.c
+++ tests/unittests/torture_isipaddr.c
@@ -9,6 +9,7 @@
  */
 static void torture_ssh_is_ipaddr(void **state) {
   (void)state;
+
   assert_int_equal(ssh_is_ipaddr("127.0.0.1"),1);
   assert_int_equal(ssh_is_ipaddr("0.0.0.0"),1);
   assert_int_equal(ssh_is_ipaddr("1.1.1.1"),1);
@@ -16,6 +17,13 @@ static void torture_ssh_is_ipaddr(void **state) {
   assert_int_equal(ssh_is_ipaddr("128.128.128.128"),1);
   assert_int_equal(ssh_is_ipaddr("1.10.100.1"),1);
   assert_int_equal(ssh_is_ipaddr("0.1.10.100"),1);
+
+  assert_int_equal(ssh_is_ipaddr("2001:0db8:85a3:0000:0000:8a2e:0370:7334"),1);
+  assert_int_equal(ssh_is_ipaddr("fe80:0000:0000:0000:0202:b3ff:fe1e:8329"),1);
+  assert_int_equal(ssh_is_ipaddr("fe80:0:0:0:202:b3ff:fe1e:8329"),1);
+  assert_int_equal(ssh_is_ipaddr("fe80::202:b3ff:fe1e:8329"),1);
+
+  assert_int_equal(ssh_is_ipaddr("::ffff:192.0.2.128"),1);
 
   assert_int_equal(ssh_is_ipaddr("0.0.0.0.0"),0);
   assert_int_equal(ssh_is_ipaddr("0.0.0.0.a"),0);
