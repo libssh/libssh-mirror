e8c3f5575173f2d0f0258992574ea3d5352fcf43
Andreas Schneider <asn@cryptomilk.org>
2011-02-12 19:12:39 +0100

    tests: Fixed torture_isipaddr.

--- tests/unittests/torture_isipaddr.c
+++ tests/unittests/torture_isipaddr.c
@@ -2,34 +2,34 @@
 
 #include "torture.h"
 
-#include "connect.c"
+#include "misc.c"
 
 /*
- * Test the behavior of isipaddr()
+ * Test the behavior of ssh_is_ipaddr()
  */
-static void torture_isipaddr(void **state) {
+static void torture_ssh_is_ipaddr(void **state) {
   (void)state;
-  assert_int_equal(isipaddr("127.0.0.1"),1);
-  assert_int_equal(isipaddr("0.0.0.0"),1);
-  assert_int_equal(isipaddr("1.1.1.1"),1);
-  assert_int_equal(isipaddr("255.255.255.255"),1);
-  assert_int_equal(isipaddr("128.128.128.128"),1);
-  assert_int_equal(isipaddr("1.10.100.1"),1);
-  assert_int_equal(isipaddr("0.1.10.100"),1);
+  assert_int_equal(ssh_is_ipaddr("127.0.0.1"),1);
+  assert_int_equal(ssh_is_ipaddr("0.0.0.0"),1);
+  assert_int_equal(ssh_is_ipaddr("1.1.1.1"),1);
+  assert_int_equal(ssh_is_ipaddr("255.255.255.255"),1);
+  assert_int_equal(ssh_is_ipaddr("128.128.128.128"),1);
+  assert_int_equal(ssh_is_ipaddr("1.10.100.1"),1);
+  assert_int_equal(ssh_is_ipaddr("0.1.10.100"),1);
 
-  assert_int_equal(isipaddr("0.0.0.0.0"),0);
-  assert_int_equal(isipaddr("0.0.0.0.a"),0);
-  assert_int_equal(isipaddr("a.0.0.0"),0);
-  assert_int_equal(isipaddr("0a.0.0.0.0"),0);
-  assert_int_equal(isipaddr(""),0);
-  assert_int_equal(isipaddr("0.0.0."),0);
-  assert_int_equal(isipaddr("0.0.0"),0);
+  assert_int_equal(ssh_is_ipaddr("0.0.0.0.0"),0);
+  assert_int_equal(ssh_is_ipaddr("0.0.0.0.a"),0);
+  assert_int_equal(ssh_is_ipaddr("a.0.0.0"),0);
+  assert_int_equal(ssh_is_ipaddr("0a.0.0.0.0"),0);
+  assert_int_equal(ssh_is_ipaddr(""),0);
+  assert_int_equal(ssh_is_ipaddr("0.0.0."),0);
+  assert_int_equal(ssh_is_ipaddr("0.0.0"),0);
 }
 
 int torture_run_tests(void) {
     int rc;
     const UnitTest tests[] = {
-        unit_test(torture_isipaddr)
+        unit_test(torture_ssh_is_ipaddr)
     };
 
     ssh_init();
