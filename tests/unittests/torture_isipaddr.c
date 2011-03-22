79b74bdf64e55f407b448fda267589f793ddc19e
Aris Adamantiadis <aris@0xbadc0de.be>
2011-03-22 10:45:07 +0100

    Added a IP address test

--- tests/unittests/torture_isipaddr.c
+++ tests/unittests/torture_isipaddr.c
@@ -35,6 +35,7 @@ static void torture_ssh_is_ipaddr(void **state) {
   assert_int_equal(ssh_is_ipaddr("0.0.0."),0);
   assert_int_equal(ssh_is_ipaddr("0.0"),0);
   assert_int_equal(ssh_is_ipaddr("0"),0);
+  assert_int_equal(ssh_is_ipaddr("255.255.255"),0);
 
   assert_int_equal(ssh_is_ipaddr("2001:0db8:85a3:0000:0000:8a2e:0370:7334:1002"), 0);
   assert_int_equal(ssh_is_ipaddr("fe80:x:202:b3ff:fe1e:8329"), 0);
