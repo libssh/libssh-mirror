81332e1e2739b997923dbe4deb07efaf60dd5fe4
Andreas Schneider <asn@cryptomilk.org>
2011-06-15 16:15:14 +0200

    build: Fix libssh_threads pkg-config. (cherry picked from commit 269c9fed354f3a8adbc54fccba6287d5b1f166e3)

--- libssh_threads.pc.cmake
+++ libssh_threads.pc.cmake
@@ -1,5 +1,5 @@
 Name: ${APPLICATION_NAME}_threads
 Description: The SSH Library Thread Extension
 Version: ${APPLICATION_VERSION}
-Libs: -L${LIB_INSTALL_DIR} -lssh -lssh_threads
+Libs: -L${LIB_INSTALL_DIR} -lssh_threads
 Cflags: -I${INCLUDE_INSTALL_DIR}
