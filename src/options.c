ad24427f038221e5276903ab599359e495127b16
Oliver Gasser <gasser@in.tum.de>
2012-06-21 19:06:47 +0200

    options: Add option to specify host keys string.
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/options.c
+++ src/options.c
@@ -334,6 +334,11 @@ int ssh_options_set_algo(ssh_session session, int algo,
  *                comma-separated list). ex:
  *                "ecdh-sha2-nistp256,diffie-hellman-group1-sha1"
  *
+ *              - SSH_OPTIONS_HOSTKEYS:
+ *                Set the preferred server host key types (const char *,
+ *                comma-separated list). ex:
+ *                "ssh-rsa,ssh-dsa,ecdh-sha2-nistp256"
+ *
  *              - SSH_OPTIONS_COMPRESSION_C_S:
  *                Set the compression to use for client to server
  *                communication (const char *, "yes", "no" or a specific
@@ -682,6 +687,16 @@ int ssh_options_set(ssh_session session, enum ssh_options_e type,
                 return -1;
             } else {
                 if (ssh_options_set_algo(session, SSH_KEX, v) < 0)
+                    return -1;
+            }
+            break;
+        case SSH_OPTIONS_HOSTKEYS:
+            v = value;
+            if (v == NULL || v[0] == '\0') {
+                ssh_set_error_invalid(session);
+                return -1;
+            } else {
+                if (ssh_options_set_algo(session, SSH_HOSTKEYS, v) < 0)
                     return -1;
             }
             break;
