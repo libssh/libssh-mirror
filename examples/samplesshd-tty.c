d9c5d0767cb50bb938146e74dcbe6c89e523b291
Andreas Schneider <asn@cryptomilk.org>
2013-12-26 09:42:32 +0100

    examples: Fix building samplesshd-tty on FreeBSD.

--- examples/samplesshd-tty.c
+++ examples/samplesshd-tty.c
@@ -42,6 +42,8 @@ clients must be made or how a client should react.
 #endif
 #endif
 
+static int port = 22;
+
 #ifdef WITH_PCAP
 const char *pcap_file="debug.server.pcap";
 ssh_pcap_file pcap;
@@ -83,8 +85,6 @@ static char doc[] = "libssh -- a Secure Shell protocol implementation";
 
 /* A description of the arguments we accept. */
 static char args_doc[] = "BINDADDR";
-
-static int port = 22;
 
 /* The options we understand. */
 static struct argp_option options[] = {
