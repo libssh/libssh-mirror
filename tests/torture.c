a228c3f7283dfedaab5d9eb56b26a7fef3eb809f
Andreas Schneider <asn@cryptomilk.org>
2016-01-21 12:23:08 +0100

    torture: Also write stderr to a file
    This allows to capture debug information of the wrappers.
    
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
    
    (cherry-picked from commit c365ff3d)

--- tests/torture.c
+++ tests/torture.c
@@ -867,8 +867,8 @@ void torture_setup_sshd_server(void **state)
     s = *state;
 
     snprintf(sshd_start_cmd, sizeof(sshd_start_cmd),
-             "/usr/sbin/sshd -r -f %s -E %s/sshd/sshd.log",
-             s->srv_config, s->socket_dir);
+             "/usr/sbin/sshd -r -f %s -E %s/sshd/daemon.log 2> %s/sshd/cwrap.log",
+             s->srv_config, s->socket_dir, s->socket_dir);
 
     rc = system(sshd_start_cmd);
     assert_return_code(rc, errno);
