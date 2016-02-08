1bc4176d657e772e29c0f4f903b5447bafd24ef0
Andreas Schneider <asn@cryptomilk.org>
2016-02-08 18:46:22 +0100

    torture: Use ECDSA key for x11 test
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- tests/unittests/torture_server_x11.c
+++ tests/unittests/torture_server_x11.c
@@ -36,7 +36,7 @@ static int setup(void **state) {
     assert_return_code(h->fd, errno);
     close(h->fd);
 
-    h->key_type = SSH_KEYTYPE_RSA;
+    h->key_type = SSH_KEYTYPE_ECDSA;
     h->hostkey = torture_get_testkey(h->key_type, 0, 0);
 
     torture_write_file(h->hostkey_path, h->hostkey);
