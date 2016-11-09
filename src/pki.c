043560d7dd4973910c632b46ec0d2d615a56effb
Justus Winter <justus@g10code.com>
2016-11-09 16:34:15 +0100

    pki_gcrypt: Fix freeing of ECDSA signatures.
    * src/pki.c (ssh_signature_free): Fix test for ECC using gcrypt.
    
    Signed-off-by: Justus Winter <justus@g10code.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/pki.c
+++ src/pki.c
@@ -357,7 +357,7 @@ void ssh_signature_free(ssh_signature sig)
 #endif
             break;
         case SSH_KEYTYPE_ECDSA:
-#ifdef HAVE_LIBGCRYPT_ECC
+#ifdef HAVE_GCRYPT_ECC
             gcry_sexp_release(sig->ecdsa_sig);
 #elif defined(HAVE_LIBCRYPTO) && defined(HAVE_OPENSSL_ECC)
             ECDSA_SIG_free(sig->ecdsa_sig);
