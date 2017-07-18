libssh/libssh-mirror

commit 48e7b098f86207f8596651b5ba8242a3b834a868

wrapper: Fix possible crash in ssh_handle_key_exchange()
If ssh_handle_key_exchange() fails, when ssh_free() is called
cipher->ctx is NULL.

Signed-off-by: debevv <valerio@sonicpengu.in>

src/wrapper.c
@@ -120,12 +120,14 @@ void ssh_cipher_clear(struct ssh_cipher_struct *cipher){
     SAFE_FREE(cipher->key);
   }
 #endif
-  if (cipher->cleanup != NULL){
-    cipher->cleanup(cipher);
-  }
+  if (cipher->ctx != NULL) {
+    if (cipher->cleanup != NULL) {
+      cipher->cleanup(cipher);
+    }
 #ifdef HAVE_LIBCRYPTO
-  EVP_CIPHER_CTX_free(cipher->ctx);
+    EVP_CIPHER_CTX_free(cipher->ctx);
 #endif
+  }
 }
 
 static void cipher_free(struct ssh_cipher_struct *cipher) {

commit a5bc81d406abd174e1779253e8dc3c0accf7dd17

wrapper: fix gcrypt build error in ssh_cipher_clear
Fix a gcrypt build error introduced with
48e7b098f86207f8596651b5ba8242a3b834a868.

The ssh_cipher_struct only contains a `ctx` field on
the libcrypto builds, so it can't be referenced unless
within HAVE_LIBCRYPTO.

This build fix preserves the original spirit of the
change in 48e7b098f86207f8596651b5ba8242a3b834a868:
only call `EVP_CIPHER_CTX_free` when `cipher->ctx`
is non-NULL.

Signed-off-by: Jon Simons <jon@jonsimons.org>
Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

src/wrapper.c
@@ -120,14 +120,16 @@ void ssh_cipher_clear(struct ssh_cipher_struct *cipher){
         SAFE_FREE(cipher->key);
     }
 #endif
-    if (cipher->ctx != NULL) {
-        if (cipher->cleanup != NULL) {
-            cipher->cleanup(cipher);
-        }
-#ifdef HAVE_LIBCRYPTO
-        EVP_CIPHER_CTX_free(cipher->ctx);
-#endif
+
+    if (cipher->cleanup != NULL) {
+        cipher->cleanup(cipher);
     }
+
+#ifdef HAVE_LIBCRYPTO
+    if (cipher->ctx != NULL) {
+        EVP_CIPHER_CTX_free(cipher->ctx);
+    }
+#endif
 }
 
 static void cipher_free(struct ssh_cipher_struct *cipher) {