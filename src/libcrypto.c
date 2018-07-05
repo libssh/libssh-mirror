38c53db9533a3e0ac446ee0a83dac8768d282de9
Andreas Schneider <asn@cryptomilk.org>
2018-07-05 08:56:33 +0200

    libcrypto: Use new RAND_priv_bytes() for strong randomness
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/libcrypto.c
+++ src/libcrypto.c
@@ -93,7 +93,14 @@ void ssh_reseed(void){
  */
 int ssh_get_random(void *where, int len, int strong)
 {
+#ifdef HAVE_OPENSSL_RAND_PRIV_BYTES
+    if (strong) {
+        /* Returns -1 when not supported, 0 on error, 1 on success */
+        return !!RAND_priv_bytes(where, len);
+    }
+#else
     (void)strong;
+#endif /* HAVE_RAND_PRIV_BYTES */
 
     /* Returns -1 when not supported, 0 on error, 1 on success */
     return !!RAND_bytes(where, len);
