139ccaa78cf4caad68c699cac608ba3f0f9fead3
Andreas Schneider <asn@cryptomilk.org>
2018-08-13 11:01:27 +0200

    include: Fix version number
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- include/libssh/libssh.h
+++ include/libssh/libssh.h
@@ -78,8 +78,8 @@
 
 /* libssh version */
 #define LIBSSH_VERSION_MAJOR  0
-#define LIBSSH_VERSION_MINOR  7
-#define LIBSSH_VERSION_MICRO  90
+#define LIBSSH_VERSION_MINOR  8
+#define LIBSSH_VERSION_MICRO  0
 
 #define LIBSSH_VERSION_INT SSH_VERSION_INT(LIBSSH_VERSION_MAJOR, \
                                            LIBSSH_VERSION_MINOR, \
