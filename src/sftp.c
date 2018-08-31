89c525bbf1ee35ae802259a580718591bfe04fdf
Andreas Schneider <asn@cryptomilk.org>
2018-08-31 18:38:42 +0200

    sftp: Reformat sftp_mkdir()
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/sftp.c
+++ src/sftp.c
@@ -2227,106 +2227,107 @@ int sftp_rmdir(sftp_session sftp, const char *directory) {
 }
 
 /* Code written by Nick */
-int sftp_mkdir(sftp_session sftp, const char *directory, mode_t mode) {
-  sftp_status_message status = NULL;
-  sftp_message msg = NULL;
-  sftp_attributes errno_attr = NULL;
-  struct sftp_attributes_struct attr;
-  ssh_buffer buffer;
-  ssh_string path;
-  uint32_t id;
-  int rc;
+int sftp_mkdir(sftp_session sftp, const char *directory, mode_t mode)
+{
+    sftp_status_message status = NULL;
+    sftp_message msg = NULL;
+    sftp_attributes errno_attr = NULL;
+    struct sftp_attributes_struct attr;
+    ssh_buffer buffer;
+    ssh_string path;
+    uint32_t id;
+    int rc;
 
-  buffer = ssh_buffer_new();
-  if (buffer == NULL) {
-    ssh_set_error_oom(sftp->session);
-    return -1;
-  }
+    buffer = ssh_buffer_new();
+    if (buffer == NULL) {
+        ssh_set_error_oom(sftp->session);
+        return -1;
+    }
 
-  path = ssh_string_from_char(directory);
-  if (path == NULL) {
-    ssh_set_error_oom(sftp->session);
-    ssh_buffer_free(buffer);
-    return -1;
-  }
+    path = ssh_string_from_char(directory);
+    if (path == NULL) {
+        ssh_set_error_oom(sftp->session);
+        ssh_buffer_free(buffer);
+        return -1;
+    }
 
-  ZERO_STRUCT(attr);
-  attr.permissions = mode;
-  attr.flags = SSH_FILEXFER_ATTR_PERMISSIONS;
+    ZERO_STRUCT(attr);
+    attr.permissions = mode;
+    attr.flags = SSH_FILEXFER_ATTR_PERMISSIONS;
 
-  rc = ssh_buffer_allocate_size(buffer,
-                                sizeof(uint32_t) * 2 +
-                                ssh_string_len(path));
-  if (rc < 0) {
-    ssh_set_error_oom(sftp->session);
+    rc = ssh_buffer_allocate_size(buffer,
+            sizeof(uint32_t) * 2 +
+            ssh_string_len(path));
+    if (rc < 0) {
+        ssh_set_error_oom(sftp->session);
+        ssh_buffer_free(buffer);
+        ssh_string_free(path);
+        return -1;
+    }
+
+    id = sftp_get_new_id(sftp);
+    if (ssh_buffer_add_u32(buffer, htonl(id)) < 0 ||
+            ssh_buffer_add_ssh_string(buffer, path) < 0 ||
+            buffer_add_attributes(buffer, &attr) < 0 ||
+            sftp_packet_write(sftp, SSH_FXP_MKDIR, buffer) < 0) {
+        ssh_buffer_free(buffer);
+        ssh_string_free(path);
+        return -1;
+    }
     ssh_buffer_free(buffer);
     ssh_string_free(path);
-    return -1;
-  }
 
-  id = sftp_get_new_id(sftp);
-  if (ssh_buffer_add_u32(buffer, htonl(id)) < 0 ||
-      ssh_buffer_add_ssh_string(buffer, path) < 0 ||
-      buffer_add_attributes(buffer, &attr) < 0 ||
-      sftp_packet_write(sftp, SSH_FXP_MKDIR, buffer) < 0) {
-    ssh_buffer_free(buffer);
-    ssh_string_free(path);
-    return -1;
-  }
-  ssh_buffer_free(buffer);
-  ssh_string_free(path);
-
-  while (msg == NULL) {
-    if (sftp_read_and_dispatch(sftp) < 0) {
-      return -1;
-    }
-    msg = sftp_dequeue(sftp, id);
-  }
-
-  /* By specification, this command only returns SSH_FXP_STATUS */
-  if (msg->packet_type == SSH_FXP_STATUS) {
-    status = parse_status_msg(msg);
-    sftp_message_free(msg);
-    if (status == NULL) {
-      return -1;
-    }
-    sftp_set_error(sftp, status->status);
-    switch (status->status) {
-      case SSH_FX_FAILURE:
-        /*
-         * mkdir always returns a failure, even if the path already exists.
-         * To be POSIX conform and to be able to map it to EEXIST a stat
-         * call is needed here.
-         */
-        errno_attr = sftp_lstat(sftp, directory);
-        if (errno_attr != NULL) {
-          SAFE_FREE(errno_attr);
-          sftp_set_error(sftp, SSH_FX_FILE_ALREADY_EXISTS);
+    while (msg == NULL) {
+        if (sftp_read_and_dispatch(sftp) < 0) {
+            return -1;
         }
-        break;
-      case SSH_FX_OK:
-        status_msg_free(status);
-        return 0;
-        break;
-      default:
-        break;
+        msg = sftp_dequeue(sftp, id);
     }
-    /*
-     * The status should be SSH_FX_OK if the command was successful, if it
-     * didn't, then there was an error
-     */
-    ssh_set_error(sftp->session, SSH_REQUEST_DENIED,
-        "SFTP server: %s", status->errormsg);
-    status_msg_free(status);
-    return -1;
-  } else {
-    ssh_set_error(sftp->session, SSH_FATAL,
-        "Received message %d when attempting to make directory",
-        msg->packet_type);
-    sftp_message_free(msg);
-  }
 
-  return -1;
+    /* By specification, this command only returns SSH_FXP_STATUS */
+    if (msg->packet_type == SSH_FXP_STATUS) {
+        status = parse_status_msg(msg);
+        sftp_message_free(msg);
+        if (status == NULL) {
+            return -1;
+        }
+        sftp_set_error(sftp, status->status);
+        switch (status->status) {
+            case SSH_FX_FAILURE:
+                /*
+                 * mkdir always returns a failure, even if the path already exists.
+                 * To be POSIX conform and to be able to map it to EEXIST a stat
+                 * call is needed here.
+                 */
+                errno_attr = sftp_lstat(sftp, directory);
+                if (errno_attr != NULL) {
+                    SAFE_FREE(errno_attr);
+                    sftp_set_error(sftp, SSH_FX_FILE_ALREADY_EXISTS);
+                }
+                break;
+            case SSH_FX_OK:
+                status_msg_free(status);
+                return 0;
+                break;
+            default:
+                break;
+        }
+        /*
+         * The status should be SSH_FX_OK if the command was successful, if it
+         * didn't, then there was an error
+         */
+        ssh_set_error(sftp->session, SSH_REQUEST_DENIED,
+                "SFTP server: %s", status->errormsg);
+        status_msg_free(status);
+        return -1;
+    } else {
+        ssh_set_error(sftp->session, SSH_FATAL,
+                "Received message %d when attempting to make directory",
+                msg->packet_type);
+        sftp_message_free(msg);
+    }
+
+    return -1;
 }
 
 /* code written by nick */
