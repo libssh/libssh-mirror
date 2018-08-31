2b524655ae749ad9258c61c03a9d26d7c884650b
Andreas Schneider <asn@cryptomilk.org>
2018-08-31 18:34:07 +0200

    sftp: Reformat sftp_open()
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
    (cherry picked from commit bfb6718b5036e955a31fafe2e08961017ae7bae4)

--- src/sftp.c
+++ src/sftp.c
@@ -1618,132 +1618,135 @@ int sftp_closedir(sftp_dir dir){
 }
 
 /* Open a file on the server. */
-sftp_file sftp_open(sftp_session sftp, const char *file, int flags,
-    mode_t mode) {
-  sftp_message msg = NULL;
-  sftp_status_message status;
-  struct sftp_attributes_struct attr;
-  sftp_file handle;
-  ssh_string filename;
-  ssh_buffer buffer;
-  sftp_attributes stat_data;
-  uint32_t sftp_flags = 0;
-  uint32_t id;
-  int rc;
+sftp_file sftp_open(sftp_session sftp,
+                    const char *file,
+                    int flags,
+                    mode_t mode)
+{
+    sftp_message msg = NULL;
+    sftp_status_message status;
+    struct sftp_attributes_struct attr;
+    sftp_file handle;
+    ssh_string filename;
+    ssh_buffer buffer;
+    sftp_attributes stat_data;
+    uint32_t sftp_flags = 0;
+    uint32_t id;
+    int rc;
 
-  buffer = ssh_buffer_new();
-  if (buffer == NULL) {
-    ssh_set_error_oom(sftp->session);
-    return NULL;
-  }
-
-  filename = ssh_string_from_char(file);
-  if (filename == NULL) {
-    ssh_set_error_oom(sftp->session);
-    ssh_buffer_free(buffer);
-    return NULL;
-  }
-
-  ZERO_STRUCT(attr);
-  attr.permissions = mode;
-  attr.flags = SSH_FILEXFER_ATTR_PERMISSIONS;
-
-  if ((flags & O_RDWR) == O_RDWR) {
-    sftp_flags |= (SSH_FXF_WRITE | SSH_FXF_READ);
-  } else if ((flags & O_WRONLY) == O_WRONLY) {
-    sftp_flags |= SSH_FXF_WRITE;
-  } else {
-    sftp_flags |= SSH_FXF_READ;
-  }
-  if ((flags & O_CREAT) == O_CREAT)
-    sftp_flags |= SSH_FXF_CREAT;
-  if ((flags & O_TRUNC) == O_TRUNC)
-    sftp_flags |= SSH_FXF_TRUNC;
-  if ((flags & O_EXCL) == O_EXCL)
-    sftp_flags |= SSH_FXF_EXCL;
-  if ((flags & O_APPEND) == O_APPEND) {
-      sftp_flags |= SSH_FXF_APPEND;
-  }
-  SSH_LOG(SSH_LOG_PACKET,"Opening file %s with sftp flags %x",file,sftp_flags);
-  rc = ssh_buffer_allocate_size(buffer,
-                                sizeof(uint32_t) * 4 +
-                                ssh_string_len(filename));
-  if (rc < 0) {
-    ssh_set_error_oom(sftp->session);
-    ssh_buffer_free(buffer);
-    ssh_string_free(filename);
-    return NULL;
-  }
-  id = sftp_get_new_id(sftp);
-  if (ssh_buffer_add_u32(buffer, htonl(id)) < 0 ||
-      ssh_buffer_add_ssh_string(buffer, filename) < 0) {
-    ssh_set_error_oom(sftp->session);
-    ssh_buffer_free(buffer);
-    ssh_string_free(filename);
-    return NULL;
-  }
-  ssh_string_free(filename);
-
-  if (ssh_buffer_add_u32(buffer, htonl(sftp_flags)) < 0 ||
-      buffer_add_attributes(buffer, &attr) < 0) {
-    ssh_set_error_oom(sftp->session);
-    ssh_buffer_free(buffer);
-    return NULL;
-  }
-  if (sftp_packet_write(sftp, SSH_FXP_OPEN, buffer) < 0) {
-    ssh_buffer_free(buffer);
-    return NULL;
-  }
-  ssh_buffer_free(buffer);
-
-  while (msg == NULL) {
-    if (sftp_read_and_dispatch(sftp) < 0) {
-      /* something nasty has happened */
-      return NULL;
-    }
-    msg = sftp_dequeue(sftp, id);
-  }
-
-  switch (msg->packet_type) {
-    case SSH_FXP_STATUS:
-      status = parse_status_msg(msg);
-      sftp_message_free(msg);
-      if (status == NULL) {
+    buffer = ssh_buffer_new();
+    if (buffer == NULL) {
+        ssh_set_error_oom(sftp->session);
         return NULL;
-      }
-      sftp_set_error(sftp, status->status);
-      ssh_set_error(sftp->session, SSH_REQUEST_DENIED,
-          "SFTP server: %s", status->errormsg);
-      status_msg_free(status);
+    }
 
-      return NULL;
-    case SSH_FXP_HANDLE:
-      handle = parse_handle_msg(msg);
-      sftp_message_free(msg);
-      if ((flags & O_APPEND) == O_APPEND) {
-        stat_data = sftp_stat(sftp, file);
-        if (stat_data == NULL) {
-            sftp_close(handle);
+    filename = ssh_string_from_char(file);
+    if (filename == NULL) {
+        ssh_set_error_oom(sftp->session);
+        ssh_buffer_free(buffer);
+        return NULL;
+    }
+
+    ZERO_STRUCT(attr);
+    attr.permissions = mode;
+    attr.flags = SSH_FILEXFER_ATTR_PERMISSIONS;
+
+    if ((flags & O_RDWR) == O_RDWR) {
+        sftp_flags |= (SSH_FXF_WRITE | SSH_FXF_READ);
+    } else if ((flags & O_WRONLY) == O_WRONLY) {
+        sftp_flags |= SSH_FXF_WRITE;
+    } else {
+        sftp_flags |= SSH_FXF_READ;
+    }
+    if ((flags & O_CREAT) == O_CREAT)
+        sftp_flags |= SSH_FXF_CREAT;
+    if ((flags & O_TRUNC) == O_TRUNC)
+        sftp_flags |= SSH_FXF_TRUNC;
+    if ((flags & O_EXCL) == O_EXCL)
+        sftp_flags |= SSH_FXF_EXCL;
+    if ((flags & O_APPEND) == O_APPEND) {
+        sftp_flags |= SSH_FXF_APPEND;
+    }
+    SSH_LOG(SSH_LOG_PACKET,"Opening file %s with sftp flags %x",file,sftp_flags);
+    rc = ssh_buffer_allocate_size(buffer,
+            sizeof(uint32_t) * 4 +
+            ssh_string_len(filename));
+    if (rc < 0) {
+        ssh_set_error_oom(sftp->session);
+        ssh_buffer_free(buffer);
+        ssh_string_free(filename);
+        return NULL;
+    }
+    id = sftp_get_new_id(sftp);
+    if (ssh_buffer_add_u32(buffer, htonl(id)) < 0 ||
+            ssh_buffer_add_ssh_string(buffer, filename) < 0) {
+        ssh_set_error_oom(sftp->session);
+        ssh_buffer_free(buffer);
+        ssh_string_free(filename);
+        return NULL;
+    }
+    ssh_string_free(filename);
+
+    if (ssh_buffer_add_u32(buffer, htonl(sftp_flags)) < 0 ||
+            buffer_add_attributes(buffer, &attr) < 0) {
+        ssh_set_error_oom(sftp->session);
+        ssh_buffer_free(buffer);
+        return NULL;
+    }
+    if (sftp_packet_write(sftp, SSH_FXP_OPEN, buffer) < 0) {
+        ssh_buffer_free(buffer);
+        return NULL;
+    }
+    ssh_buffer_free(buffer);
+
+    while (msg == NULL) {
+        if (sftp_read_and_dispatch(sftp) < 0) {
+            /* something nasty has happened */
             return NULL;
         }
-        if ((stat_data->flags & SSH_FILEXFER_ATTR_SIZE) != SSH_FILEXFER_ATTR_SIZE) {
-          ssh_set_error(sftp->session,
-                        SSH_FATAL,
-                        "Cannot open in append mode. Unknown file size.");
-          sftp_close(handle);
-          return NULL;
-        }
+        msg = sftp_dequeue(sftp, id);
+    }
 
-        handle->offset = stat_data->size;
-      }
-      return handle;
-    default:
-      ssh_set_error(sftp->session, SSH_FATAL,
-          "Received message %d during open!", msg->packet_type);
-      sftp_message_free(msg);
-  }
+    switch (msg->packet_type) {
+        case SSH_FXP_STATUS:
+            status = parse_status_msg(msg);
+            sftp_message_free(msg);
+            if (status == NULL) {
+                return NULL;
+            }
+            sftp_set_error(sftp, status->status);
+            ssh_set_error(sftp->session, SSH_REQUEST_DENIED,
+                    "SFTP server: %s", status->errormsg);
+            status_msg_free(status);
 
-  return NULL;
+            return NULL;
+        case SSH_FXP_HANDLE:
+            handle = parse_handle_msg(msg);
+            sftp_message_free(msg);
+            if ((flags & O_APPEND) == O_APPEND) {
+                stat_data = sftp_stat(sftp, file);
+                if (stat_data == NULL) {
+                    sftp_close(handle);
+                    return NULL;
+                }
+                if ((stat_data->flags & SSH_FILEXFER_ATTR_SIZE) != SSH_FILEXFER_ATTR_SIZE) {
+                    ssh_set_error(sftp->session,
+                            SSH_FATAL,
+                            "Cannot open in append mode. Unknown file size.");
+                    sftp_close(handle);
+                    return NULL;
+                }
+
+                handle->offset = stat_data->size;
+            }
+            return handle;
+        default:
+            ssh_set_error(sftp->session, SSH_FATAL,
+                    "Received message %d during open!", msg->packet_type);
+            sftp_message_free(msg);
+    }
+
+    return NULL;
 }
 
 void sftp_file_set_nonblocking(sftp_file handle){
