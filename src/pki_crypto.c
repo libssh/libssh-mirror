1acb82e38a7969115e439af148d1d6a703ddba72
Andreas Schneider <asn@cryptomilk.org>
2018-09-03 17:51:43 +0200

    pki_crypto: Use explicit_bzero()
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>

--- src/pki_crypto.c
+++ src/pki_crypto.c
@@ -1230,7 +1230,7 @@ static ssh_string _RSA_do_sign_hash(const unsigned char *digest,
     }
 
     ssh_string_fill(sig_blob, sig, slen);
-    memset(sig, 'd', slen);
+    explicit_bzero(sig, slen);
     SAFE_FREE(sig);
 
     return sig_blob;
