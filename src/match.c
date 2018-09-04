e9b44d26b12aa1314718a37fe2598dafb9843e3f
Jakub Jelen <jjelen@redhat.com>
2018-09-04 13:46:37 +0200

    config: Parse Match keyword
    Amends f818e63f8, which introduced the constants and matching of this
    configuration option, but did not implement the handling of the values
    which was causing the configuration parser failing for certain
    configurations.
    
    This commit exposes match_pattern_list() from match.c
    
    Red Hat Bugzilla:
    https://bugzilla.redhat.com/show_bug.cgi?id=1624425
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/match.c
+++ src/match.c
@@ -119,7 +119,7 @@ static int match_pattern(const char *s, const char *pattern) {
  * Returns -1 if negation matches, 1 if there is a positive match, 0 if there is
  * no match at all.
  */
-static int match_pattern_list(const char *string, const char *pattern,
+int match_pattern_list(const char *string, const char *pattern,
     unsigned int len, int dolower) {
   char sub[1024];
   int negated;
