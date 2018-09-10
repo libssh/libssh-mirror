466bb332c153e8394b15be49b2cc5ce76a1fbddf
Andreas Schneider <asn@cryptomilk.org>
2018-09-10 10:29:08 +0200

    sftp: Reformat sftp_free()
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
    (cherry picked from commit d840a05be32de74f920f4d81b60da2de8b4dce5d)

--- src/sftp.c
+++ src/sftp.c
@@ -244,31 +244,32 @@ int sftp_server_init(sftp_session sftp){
 }
 #endif /* WITH_SERVER */
 
-void sftp_free(sftp_session sftp){
-  sftp_request_queue ptr;
+void sftp_free(sftp_session sftp)
+{
+    sftp_request_queue ptr;
 
-  if (sftp == NULL) {
-    return;
-  }
+    if (sftp == NULL) {
+        return;
+    }
 
-  ssh_channel_send_eof(sftp->channel);
-  ptr = sftp->queue;
-  while(ptr) {
-    sftp_request_queue old;
-    sftp_message_free(ptr->message);
-    old = ptr->next;
-    SAFE_FREE(ptr);
-    ptr = old;
-  }
+    ssh_channel_send_eof(sftp->channel);
+    ptr = sftp->queue;
+    while(ptr) {
+        sftp_request_queue old;
+        sftp_message_free(ptr->message);
+        old = ptr->next;
+        SAFE_FREE(ptr);
+        ptr = old;
+    }
 
-  ssh_channel_free(sftp->channel);
+    ssh_channel_free(sftp->channel);
 
-  SAFE_FREE(sftp->handles);
+    SAFE_FREE(sftp->handles);
 
-  sftp_ext_free(sftp->ext);
-  ZERO_STRUCTP(sftp);
+    sftp_ext_free(sftp->ext);
+    ZERO_STRUCTP(sftp);
 
-  SAFE_FREE(sftp);
+    SAFE_FREE(sftp);
 }
 
 int sftp_packet_write(sftp_session sftp, uint8_t type, ssh_buffer payload){
