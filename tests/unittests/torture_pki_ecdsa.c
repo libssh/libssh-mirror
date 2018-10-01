cf3c2ee5b3a48bd3388593036b10cae8b69bcdc1
Andreas Schneider <asn@cryptomilk.org>
2018-10-01 20:55:47 +0200

    tests: Improve asserts in torture_pki_ecdsa
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- tests/unittests/torture_pki_ecdsa.c
+++ tests/unittests/torture_pki_ecdsa.c
@@ -336,14 +336,15 @@ static void torture_pki_ecdsa_duplicate_then_demote(void **state)
                                      NULL,
                                      NULL,
                                      &privkey);
-    assert_true(rc == 0);
+    assert_int_equal(rc, 0);
 
     privkey_dup = ssh_key_dup(privkey);
-    assert_true(privkey_dup != NULL);
+    assert_non_null(privkey_dup);
     assert_int_equal(privkey->ecdsa_nid, privkey_dup->ecdsa_nid);
 
     rc = ssh_pki_export_privkey_to_pubkey(privkey_dup, &pubkey);
-    assert_true(rc == 0);
+    assert_int_equal(rc, 0);
+    assert_non_null(pubkey);
     assert_int_equal(pubkey->ecdsa_nid, privkey->ecdsa_nid);
 
     SSH_KEY_FREE(pubkey);
