836982358a87f5b8611a95331bfad5cddd4d6c58
Jakub Jelen <jjelen@redhat.com>
2018-11-15 11:03:56 +0100

    packet: Adjust the packet filter to allow client-initialized rekey
    If the rekey is initialized by client, it sends the first KEXINIT
    message, changes to the INIT_SENT state and waits for the KEXINIT
    message from the server. This was not covered in the current filter.
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Daiki Ueno <dueno@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/packet.c
+++ src/packet.c
@@ -292,6 +292,7 @@ static enum ssh_packet_filter_result_e ssh_packet_incoming_filter(ssh_session se
          * - session_state == SSH_SESSION_STATE_AUTHENTICATED
          *   or session_state == SSH_SESSION_STATE_INITIAL_KEX
          * - dh_handshake_state == DH_STATE_INIT
+         *   or dh_handshake_state == DH_STATE_INIT_SENT (re-exchange)
          *   or dh_handshake_state == DH_STATE_FINISHED (re-exchange)
          *
          * Transitions:
@@ -310,6 +311,7 @@ static enum ssh_packet_filter_result_e ssh_packet_incoming_filter(ssh_session se
         }
 
         if ((session->dh_handshake_state != DH_STATE_INIT) &&
+            (session->dh_handshake_state != DH_STATE_INIT_SENT) &&
             (session->dh_handshake_state != DH_STATE_FINISHED))
         {
             rc = SSH_PACKET_DENIED;
