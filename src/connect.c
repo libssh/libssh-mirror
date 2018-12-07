58113d489eecfeb266b82de7bb25d4a0526b9606
Andreas Schneider <asn@cryptomilk.org>
2018-12-07 12:07:13 +0100

    connect: Fix size type for i an j in ssh_select()
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/connect.c
+++ src/connect.c
@@ -476,7 +476,7 @@ int ssh_select(ssh_channel *channels, ssh_channel *outchannels, socket_t maxfd,
     fd_set *readfds, struct timeval *timeout) {
   fd_set origfds;
   socket_t fd;
-  int i,j;
+  size_t i, j;
   int rc;
   int base_tm, tm;
   struct ssh_timestamp ts;
