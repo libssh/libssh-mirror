bdf968c178d3cb54c0f5f968e2030926614cae21
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2019-01-15 13:19:36 +0100

    tests: Added NULL checks in torture_list.c
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- tests/unittests/torture_list.c
+++ tests/unittests/torture_list.c
@@ -33,16 +33,32 @@ static void torture_ssh_list_append(void **state) {
 
     rc = ssh_list_append(xlist, "item1");
     assert_true(rc == 0);
+    assert_non_null(xlist->root);
+    assert_non_null(xlist->root->data);
+    assert_non_null(xlist->end);
+    assert_non_null(xlist->end->data);
     assert_string_equal((const char *) xlist->root->data, "item1");
     assert_string_equal((const char *) xlist->end->data, "item1");
 
     rc = ssh_list_append(xlist, "item2");
     assert_true(rc == 0);
+    assert_non_null(xlist->root);
+    assert_non_null(xlist->root->data);
+    assert_non_null(xlist->end);
+    assert_non_null(xlist->end->data);
     assert_string_equal((const char *) xlist->root->data, "item1");
     assert_string_equal((const char *) xlist->end->data, "item2");
 
     rc = ssh_list_append(xlist, "item3");
     assert_true(rc == 0);
+    assert_non_null(xlist->root);
+    assert_non_null(xlist->root->data);
+    assert_non_null(xlist->root->next);
+    assert_non_null(xlist->root->next->data);
+    assert_non_null(xlist->root->next->next);
+    assert_non_null(xlist->root->next->next->data);
+    assert_non_null(xlist->end);
+    assert_non_null(xlist->end->data);
     assert_string_equal((const char *) xlist->root->data, "item1");
     assert_string_equal((const char *) xlist->root->next->data, "item2");
     assert_string_equal((const char *) xlist->root->next->next->data, "item3");
@@ -64,16 +80,31 @@ static void torture_ssh_list_prepend(void **state) {
 
     rc = ssh_list_prepend(xlist, "item1");
     assert_true(rc == 0);
+    assert_non_null(xlist->root);
+    assert_non_null(xlist->root->data);
+    assert_non_null(xlist->end);
+    assert_non_null(xlist->end->data);
     assert_string_equal((const char *) xlist->root->data, "item1");
     assert_string_equal((const char *) xlist->end->data, "item1");
 
     rc = ssh_list_append(xlist, "item2");
     assert_true(rc == 0);
+    assert_non_null(xlist->root);
+    assert_non_null(xlist->root->data);
+    assert_non_null(xlist->end);
+    assert_non_null(xlist->end->data);
     assert_string_equal((const char *) xlist->root->data, "item1");
     assert_string_equal((const char *) xlist->end->data, "item2");
 
     rc = ssh_list_prepend(xlist, "item3");
     assert_true(rc == 0);
+    assert_non_null(xlist->root);
+    assert_non_null(xlist->root->data);
+    assert_non_null(xlist->root->next);
+    assert_non_null(xlist->root->next->data);
+    assert_non_null(xlist->root->next->next);
+    assert_non_null(xlist->end);
+    assert_non_null(xlist->end->data);
     assert_string_equal((const char *) xlist->root->data, "item3");
     assert_string_equal((const char *) xlist->root->next->data, "item1");
     assert_string_equal((const char *) xlist->root->next->next->data, "item2");
