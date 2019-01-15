8ddbe7bec623e3ab16f654b7aab7a0e74781f73d
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2019-01-15 16:08:31 +0100

    sftp: Add NULL check in sftp_ext_free()
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/sftp.c
+++ src/sftp.c
@@ -81,19 +81,26 @@ static sftp_ext sftp_ext_new(void) {
 
 static void sftp_ext_free(sftp_ext ext)
 {
-    unsigned int i;
+    size_t i;
 
     if (ext == NULL) {
         return;
     }
 
-    if (ext->count) {
-        for (i = 0; i < ext->count; i++) {
-            SAFE_FREE(ext->name[i]);
-            SAFE_FREE(ext->data[i]);
+    if (ext->count > 0) {
+        if (ext->name != NULL) {
+            for (i = 0; i < ext->count; i++) {
+                SAFE_FREE(ext->name[i]);
+            }
+            SAFE_FREE(ext->name);
         }
-        SAFE_FREE(ext->name);
-        SAFE_FREE(ext->data);
+
+        if (ext->data != NULL) {
+            for (i = 0; i < ext->count; i++) {
+                SAFE_FREE(ext->data[i]);
+            }
+            SAFE_FREE(ext->data);
+        }
     }
 
     SAFE_FREE(ext);
