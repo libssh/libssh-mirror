b73ffb3f91ea26412482d145512e4261df903df7
David Wedderwille <davidwe@posteo.de>
2019-02-14 16:14:14 +0100

    connector: Fallback on the socket output callback
    Fixes T124
    
    Signed-off-by: David Wedderwille <davidwe@posteo.de>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/server.c
+++ src/server.c
@@ -314,6 +314,7 @@ static void ssh_server_connection_callback(ssh_session session){
 
             /* from now, the packet layer is handling incoming packets */
             session->socket_callbacks.data=ssh_packet_socket_callback;
+            ssh_packet_register_socket_callback(session, session->socket);
 
             ssh_packet_set_default_callbacks(session);
             set_status(session, 0.5f);
