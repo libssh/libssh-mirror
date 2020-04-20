libssh/libssh-mirror

commit 3bad0607384b1fd92f96ba3db5b65d094b6db8c2

channels: Use a size_t variable for length check
Fixes T188

Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
Reviewed-by: Jakub Jelen <jjelen@redhat.com>

src/channels.c
@@ -3082,6 +3082,7 @@ int ssh_channel_poll_timeout(ssh_channel channel, int timeout, int is_stderr)
     ssh_session session;
     ssh_buffer stdbuf;
     struct ssh_channel_read_termination_struct ctx;
+    size_t len;
     int rc;
 
     if(channel == NULL) {
@@ -3106,8 +3107,8 @@ int ssh_channel_poll_timeout(ssh_channel channel, int timeout, int is_stderr)
         rc = SSH_ERROR;
         goto out;
     }
-    rc = ssh_buffer_get_len(stdbuf);
-    if(rc > 0) {
+    len = ssh_buffer_get_len(stdbuf);
+    if (len > 0) {
         goto out;
     }
     if (channel->remote_eof) {

commit 6417f5a3cac8537ac6f6ff7fc1642dfaa0917fb4

channels: Avoid returning SSH_AGAIN from ssh_channel_poll_timeout()
This addresses a regression introduced in 3bad0607, partially fixed in 022409e9,
but the function was still able to return SSH_AGAIN, which was not expected by
callers.

Based on discussion in [1] and [2]

[1] https://gitlab.com/libssh/libssh-mirror/-/merge_requests/101
[2] https://www.libssh.org/archive/libssh/2020-03/0000029.html

Signed-off-by: Jakub Jelen <jjelen@redhat.com>
Reviewed-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

src/channels.c
@@ -3116,6 +3116,12 @@ int ssh_channel_poll_timeout(ssh_channel channel, int timeout, int is_stderr)
         session->session_state == SSH_SESSION_STATE_ERROR) {
         rc = SSH_ERROR;
         goto out;
+    } else if (rc == SSH_AGAIN) {
+        /* If the above timeout expired, it is ok and we do not need to
+         * attempt to check the read buffer. The calling functions do not
+         * expect us to return SSH_AGAIN either here. */
+        rc = SSH_OK;
+        goto out;
     }
     len = ssh_buffer_get_len(stdbuf);
     if (len > 0) {