libssh/libssh-mirror

commit 3bad0607384b1fd92f96ba3db5b65d094b6db8c2

channels: Use a size_t variable for length check
Fixes T188

Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
Reviewed-by: Jakub Jelen <jjelen@redhat.com>

src/channels.c
@@ -3082,6 +3082,7 @@ int ssh_channel_poll_timeout(ssh_channel channel, int timeout, int is_stderr)
     ssh_session session;
     ssh_buffer stdbuf;
     struct ssh_channel_read_termination_struct ctx;
+    size_t len;
     int rc;
 
     if(channel == NULL) {
@@ -3106,8 +3107,8 @@ int ssh_channel_poll_timeout(ssh_channel channel, int timeout, int is_stderr)
         rc = SSH_ERROR;
         goto out;
     }
-    rc = ssh_buffer_get_len(stdbuf);
-    if(rc > 0) {
+    len = ssh_buffer_get_len(stdbuf);
+    if (len > 0) {
         goto out;
     }
     if (channel->remote_eof) {

commit 99e0ad75a6d1d5b6dbaaa57204af1f6d71a5dc72

tests: add testcases for ssh_channel_poll_timeout()
This adds testcases for the regression introduced in 3bad0607.

Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
Reviewed-by: Jakub Jelen <jjelen@redhat.com>

tests/client/torture_session.c
@@ -118,10 +118,64 @@ static void torture_channel_read_error(void **state) {
     ssh_channel_free(channel);
 }
 
+static void torture_channel_poll_timeout_valid(void **state) {
+    struct torture_state *s = *state;
+    ssh_session session = s->ssh.session;
+    ssh_channel channel;
+    int rc;
+
+    channel = ssh_channel_new(session);
+    assert_non_null(channel);
+
+    rc = ssh_channel_open_session(channel);
+    assert_ssh_return_code(session, rc);
+
+    rc = ssh_channel_request_exec(channel, "echo -n ABCD");
+    assert_ssh_return_code(session, rc);
+
+    rc = ssh_channel_poll_timeout(channel, 500, 0);
+    assert_int_equal(rc, strlen("ABCD"));
+}
+
+static void torture_channel_poll_timeout(void **state) {
+    struct torture_state *s = *state;
+    ssh_session session = s->ssh.session;
+    ssh_channel channel;
+    int rc;
+    int fd;
+
+    channel = ssh_channel_new(session);
+    assert_non_null(channel);
+
+    rc = ssh_channel_open_session(channel);
+    assert_ssh_return_code(session, rc);
+
+    fd = ssh_get_fd(session);
+    assert_true(fd > 2);
+
+    rc = ssh_channel_poll_timeout(channel, 500, 0);
+    assert_int_equal(rc, SSH_OK);
+
+    /* send crap and for server to send us a disconnect */
+    rc = write(fd, "AAAA", 4);
+    assert_int_equal(rc, 4);
+
+    rc = ssh_channel_poll_timeout(channel, 500, 0);
+    assert_int_equal(rc, SSH_ERROR);
+
+    ssh_channel_free(channel);
+}
+
 int torture_run_tests(void) {
     int rc;
     struct CMUnitTest tests[] = {
         cmocka_unit_test_setup_teardown(torture_channel_read_error,
+                                        session_setup,
+                                        session_teardown),
+        cmocka_unit_test_setup_teardown(torture_channel_poll_timeout_valid,
+                                        session_setup,
+                                        session_teardown),
+        cmocka_unit_test_setup_teardown(torture_channel_poll_timeout,
                                         session_setup,
                                         session_teardown),
     };