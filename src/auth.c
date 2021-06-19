7dfed5c3cb8278dc35996d5912b8c47cf4211372
Serdar Sanli <mserdarsanli@gmail.com>
2021-06-19 19:39:31 +0100

    Fix error in documentation
    Signed-off-by: Serdar Sanli <mserdarsanli@gmail.com>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>

--- src/auth.c
+++ src/auth.c
@@ -1044,8 +1044,7 @@ int ssh_userauth_publickey_auto_get_current_identity(ssh_session session,
  *                            method.\n
  *          SSH_AUTH_PARTIAL: You've been partially authenticated, you still
  *                            have to use another method.\n
- *          SSH_AUTH_SUCCESS: The public key is accepted, you want now to use
- *                            ssh_userauth_publickey().\n
+ *          SSH_AUTH_SUCCESS: Authentication success\n
  *          SSH_AUTH_AGAIN:   In nonblocking mode, you've got to call this again
  *                            later.
  *
