8343a43edc3571b1b16da819e06a4ecdcf217569
Norbert Pocs <npocs@redhat.com>
2022-06-30 20:29:30 +0200

    Change cmake files for new openssl API
    The new API does not provide EVP_KDF_CTX_new_id function, insted
    it works with EVP_KDF_CTX_new and fetching the algorithm.
    Adding a check for both to make it work with the new API too.
    
    Signed-off-by: Norbert Pocs <npocs@redhat.com>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>

--- config.h.cmake
+++ config.h.cmake
@@ -114,8 +114,8 @@
 /* Define to 1 if you have the `EVP_chacha20' function. */
 #cmakedefine HAVE_OPENSSL_EVP_CHACHA20 1
 
-/* Define to 1 if you have the `EVP_KDF_CTX_new_id' function. */
-#cmakedefine HAVE_OPENSSL_EVP_KDF_CTX_NEW_ID 1
+/* Define to 1 if you have the `EVP_KDF_CTX_new_id' or `EVP_KDF_CTX_new` function. */
+#cmakedefine HAVE_OPENSSL_EVP_KDF_CTX 1
 
 /* Define to 1 if you have the `FIPS_mode' function. */
 #cmakedefine HAVE_OPENSSL_FIPS_MODE 1
