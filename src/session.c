aa1e136ea35070c931142a7e12c9333930e75ced
Jakub Jelen <jjelen@redhat.com>
2022-08-05 12:22:13 +0200

    session: Avoid memory leak of agent_socket from configuration file
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=48268
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/session.c
+++ src/session.c
@@ -301,6 +301,7 @@ void ssh_free(ssh_session session)
   SAFE_FREE(session->banner);
   SAFE_FREE(session->disconnect_message);
 
+  SAFE_FREE(session->opts.agent_socket);
   SAFE_FREE(session->opts.bindaddr);
   SAFE_FREE(session->opts.custombanner);
   SAFE_FREE(session->opts.moduli_file);
